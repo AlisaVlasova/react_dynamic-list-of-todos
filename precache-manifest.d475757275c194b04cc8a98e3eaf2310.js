a1739d947386097943ebacf90ff49d7eba43dd3d
Alisa <cheshyr.alisa@gmail.com>
2020-09-09 17:08:19 +0300

    Updates

---
+++ precache-manifest.d475757275c194b04cc8a98e3eaf2310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341578f6cf0bfdddccc6e787be9c92d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b4b8a35048ed038e622",
+    "url": "./static/css/main.61424049.chunk.css"
+  },
+  {
+    "revision": "5045c5c9243450a8ed5e",
+    "url": "./static/js/2.370a55bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.370a55bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4b8a35048ed038e622",
+    "url": "./static/js/main.861da4cf.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
